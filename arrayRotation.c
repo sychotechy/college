a2c476fab38ad46e54975c6cb982835964cef46a
mec <mec@ccf-20.mec.ac.in>
2023-10-12 10:07:32 +0530

    Array rotation complete

--- arrayRotation.c
+++ arrayRotation.c
@@ -1,46 +1,72 @@
 #include<stdio.h>
-void printArray(int numberArray[],int limit){
+void printArray(int numberArray[],int maxSize){
 	int i;
-	printf("Array elements : ");
-	for(i=0;i<limit;i++){
+	printf("\n***** Array Elements *****\n");
+	for(i=0;i<maxSize;i++){
 		printf("%d\t",numberArray[i]);
 	}
+	printf("\n");
 }
-void rotateArray(int numberArray[],int limit,int direction,int noOfRotation){
-	if(direction==1){
-		
+void rotateArray(int numberArray[],int maxSize,int direction,int noOfRotation){
+	int position=1,i;
+	switch(direction){
+		case 1:
+			while(position<=noOfRotation){
+				int lastElement = numberArray[0];
+				for(i=0;i<maxSize-1;i++){
+					numberArray[i] = numberArray[i+1];
+				}
+				numberArray[maxSize-1] = lastElement;
+				position++;
+			}
+			break;
+			
+		case 2:
+			while(position<=noOfRotation){
+				int firstElement = numberArray[maxSize-1];
+				for(i=maxSize-1;i>0;i--){
+					numberArray[i] = numberArray[i-1];
+				}
+				numberArray[0] = firstElement;
+				position++;
+			}
+			break;
+			
+		default:
+			printf("\n***** Wrong option*****\n");
 	}
+		
 }
 int main(){
-	int numberArray[20],limit,i,selectedOption,flag=1,noOfRotation,direction;
-	printf("Enter the size of array : ");
-	scanf("%d",&limit);
-	printf("Enter %d elements : ");
-	for(i=0;i<limit;i++){
+	int numberArray[50],maxSize,i,flag=1,userOption,direction,noOfRotation;
+	printf("Enter size of the array : ");
+	scanf("%d",&maxSize);
+	printf("Enter %d elements : ",maxSize);
+	for(i=0;i<maxSize;i++){
 		scanf("%d",&numberArray[i]);
 	}
 	do{
-		printf("\n***** Select Operation *****\n1.Rotate Array\n2.Print Array\n3.Exit\nEnter option : ");
-		scanf("%d",&selectedOption);
-		switch(selectedOption){
+		printf("**** Operations ****\n1.Rotate array\n2.Print array\n3.Exit\nEnter your option : ");
+		scanf("%d",&userOption);
+		switch(userOption){
 			case 1:
-				printf("Enter number of rotation : ");
-				scanf("%d",&noOfRotation);
-				printf("Enter direction of rotation (1.Left 2.Right) : ");
+				printf("Enter direction to rotate (1-Left, 2-Right) : ");
 				scanf("%d",&direction);
-				rotateArray(numberArray,limit,direction,noOfRotation);
+				printf("Enter number of rotations : ");
+				scanf("%d",&noOfRotation);
+				rotateArray(numberArray,maxSize,direction,noOfRotation);
 				break;
 				
 			case 2:
-				printArray(numberArray,limit);
+				printArray(numberArray,maxSize);
 				break;
-
+				
 			case 3:
 				flag=0;
 				break;
-
+				
 			default:
-				printf("\n**** Wrong option ****\n");
+				printf("\n***** Wrong option *****\n");
 		}
 	}while(flag);
 }
